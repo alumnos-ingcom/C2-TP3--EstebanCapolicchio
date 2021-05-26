30bf5fd361e4e119293113323d18f3ca5765bd94
EstebanCapolicchio <84167593+EstebanCapolicchio@users.noreply.github.com>
2021-05-26 18:30:16 -0300

    added helping text for function "signo(num)"

--- tp4ej5.py
+++ tp4ej5.py
@@ -1,5 +1,8 @@
 #tp4ej5.py
 def signo(numero):
+    '''
+    devuelve si el numero es "mayor a 0", "menor a 0" ó "igual a 0"
+    '''
     if(numero < 0):
         return "numero negativo"
     elif(numero > 0):
