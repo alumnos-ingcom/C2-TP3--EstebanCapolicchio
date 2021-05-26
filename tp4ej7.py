b5181c866e736b82890c2d2e2d9375a90f0d0dc9
EstebanCapolicchio <84167593+EstebanCapolicchio@users.noreply.github.com>
2021-05-26 19:15:54 -0300

    added function "division_lenta(num1,num2)"

---
+++ tp4ej7.py
@@ -0,0 +1,37 @@
+#tp4ej7.py    
+def division_lenta(dividendo, divisor):
+    #nunca se puede dividir por 0...
+    if(divisor == 0):
+        raise ZeroDivisionError
+    
+    cociente = 0
+    resto = 0
+    # cualquier numero entero se puede escribir como:
+    # A = B * C + R
+    # donde:
+    # dividendo == A
+    # divisor   == B
+    # cociente  == C
+    # resto     == R
+    
+    flag_A_negativo = 0
+    flag_B_negativo = 0
+    #esto nos servirá para hacer mas facil la "division" y para asignar los signos al final
+    if(dividendo < 0):
+        dividendo = -dividendo
+        flag_A_negativo = 1
+    if(divisor < 0):
+        divisor = -divisor
+        flag_B_negativo = 1
+    while (dividendo - divisor) > 0:
+        dividendo -= divisor
+        cociente += 1
+    #¿quien se lleva los signos?
+    # si A (+) y B (+) --> C (+) y R (+) {ejemplo: 11 = 5 * 2 + 1}
+    # si A (+) y B (-) --> C (-) y R (+) {ejemplo: 11 = -5 * -2 + 1}
+    # si A (-) y B (+) --> C (-) y R (-) {ejemplo: -11 = 5 * -2 - 1}
+    # si A (-) y B (-) --> C (+) y R (-) {ejemplo: -11 = -5 * 2 - 1}
+    resto = dividendo * -1**flag_A_negativo
+    cociente = cociente * -1**flag_A_negativo * -1**flag_B_negativo
+    resultados = [cociente, resto]
+    return resultados
