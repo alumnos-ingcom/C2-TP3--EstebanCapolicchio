3420058fef494a4963247ee62e58e3d921317cfb
EstebanCapolicchio <84167593+EstebanCapolicchio@users.noreply.github.com>
2021-05-26 18:15:54 -0300

    added function "signo(num)"

---
+++ tp4ej5.py
@@ -0,0 +1,8 @@
+#tp4ej5.py
+def signo(numero):
+    if(numero < 0):
+        return "numero negativo"
+    elif(numero > 0):
+        return "numero positivo"
+    else:
+        return "es el cero"
