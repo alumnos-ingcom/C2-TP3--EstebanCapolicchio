5ebdc433e401dde63b7bcdec0d0521e4df0408fd
EstebanCapolicchio <84167593+EstebanCapolicchio@users.noreply.github.com>
2021-05-26 18:12:41 -0300

    added functions "convertir_a_fahrenheit(num)" & "convertir_a_celsius(num)"

---
+++ tp4ej3.py
@@ -0,0 +1,12 @@
+#tp4ej3.py
+def convertir_a_fahrenheit(centigrados):
+    '''
+    convierte °C -> °F
+    '''
+    return centigrados * 9/5 + 32
+    
+def convertir_a_centigrados(fahrenheit):
+    '''
+    convierte °F -> °C
+    '''
+    return (fahrenheit - 32) * 5/9
