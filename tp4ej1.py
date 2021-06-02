1ddbd5e48a034ba4468852d3aa9108dff649cd77
EstebanCapolicchio <84167593+EstebanCapolicchio@users.noreply.github.com>
2021-06-01 21:08:48 -0300

    Finished ej1
    added testing function

--- tp4ej1.py
+++ tp4ej1.py
@@ -1,15 +1,85 @@
-def ingrese_numero():
-    ingresado = input("ingrese numero entero: #")
+#tp4ej1.py
+class IngresoIncorrecto(Exception):
+    """Esta es la Excepcion para el ingreso incorrecto"""
+    pass
+
+class FueraDeLimite(Exception):
+    """Esta es la Excepcion para el ingreso incorrecto por salirse de los limites"""
+    pass
+
+def ingrese_numero(mensaje):
+    '''
+    -mensaje es un string el cual se concatenara con ": "
+    para luego pedir un numero al usuario.
+    -en caso de no ingresar un numero se levanta la siguiente excepcion:
+        IngresoIncorrecto(Exception)
+    '''
+    ingresado = input(mensaje+":")
     try:
         entero = int(ingresado)
         return entero
     except ValueError as err:
-        print("El numero ingresado no era entero")
-        ##raise IngresoIncorrecto("No era un numero") from err ####<-----------------------------------------------------------------------
+        raise IngresoIncorrecto("el input no era un numero")
 
-def ingrese_entero_reintento(intentos):
-    i = 0
-    while i < intentos:
-        entero = ingrese_numero()
-        print(entero)
-        i +=1
+def ingrese_entero_reintento(mensaje, intentos=3):#por defecto son 3 intentos
+    '''
+    -mensaje es un string el cual se utiliza en la funcion:
+        ingrese_numero(mensaje)
+    -intentos es un integer con valor por defecto igual a 3 el cual
+    al llegar a 0 sin haber ingresado correctamente un numero
+    levanta la siguiente excepcion:
+        IngresoIncorrecto(Exception)
+    '''
+    i = intentos
+    while i > 0:
+        try:
+            entero = ingrese_numero(mensaje)
+            return entero
+        except IngresoIncorrecto:
+            print("quedan "+str(i - 1)+" intentos")
+            i -=1
+    raise IngresoIncorrecto("el input no era un numero")
+
+def ingreso_entero_restringido(mensaje, valor_minimo=0, valor_maximo=10):
+    '''
+    -mensaje es un string el cual se utiliza en la funcion:
+        ingrese_numero(mensaje)
+    -valor_minimo es un integer con valor por defecto igual a 0
+    -valor_minimo es un integer con valor por defecto igual a 10
+    - si el input no es un numero levanta la siguiente excepcion:
+        IngresoIncorrecto(Exception)
+    - si el input es un numero pero esta fuera de las cotas asignadas levanta la siguiente excepcion:
+        FueraDeLimite(Exception)
+    '''
+    try:
+        entero = ingrese_numero(mensaje)
+        if(valor_minimo < entero and entero < valor_maximo):
+            return entero
+        else:
+            raise FueraDeLimite("el input esta fuera de los limites establecidos")
+    except IngresoIncorrecto:
+        raise IngresoIncorrecto("el input no era un numero")
+        
+        
+def prueba():
+    mensaje="Ingrese un numero por favor"
+    print("***************ingreso de numero entero*****************")
+    ingrese_numero(mensaje)
+    print("********************************************************")
+    print("*********ingreso de numero entero con intentos**********")
+        respuesta = str(input("quiere elegir usted cuantos intentos tiene? (Si | No) : "))
+        if(respuesta == Si || respuesta == si || respuesta==SI):
+            intentos=ingrese_numero(mensaje)
+            ingrese_entero_reintento(mensaje,intentos)
+        elif(respuesta == No || respuesta == no || respuesta==NO):
+            ingrese_entero_reintento(mensaje)#por defecto serán 3 intentos
+        else:
+            print("a ver... solo tenias que responder 'si' o 'no'...")
+            print("por ende asumiré que no te importa... pasemos a la siguiente prueba")
+    print("********************************************************")
+    print("****ingreso de numero entero entre ciertos valores******")
+    ingreso_entero_restringido(mensaje)#por defecto será entre 0 y 10
+    print("********************************************************")
+
+if __name__ == "__main__":
+    prueba()
