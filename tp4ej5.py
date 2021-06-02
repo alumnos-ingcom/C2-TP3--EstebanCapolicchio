036d4b4e2f58236e6e9ff4d3caf7166ae7ea047a
EstebanCapolicchio <84167593+EstebanCapolicchio@users.noreply.github.com>
2021-06-01 21:48:06 -0300

    Finished ej5
    added testing function
    modularized function "signo(numero)"

--- tp4ej5.py
+++ tp4ej5.py
@@ -1,11 +1,23 @@
+from tp4ej1 import ingrese_numero
+from tp4ej4 import compara
+
 #tp4ej5.py
 def signo(numero):
     '''
-    devuelve si el numero es "mayor a 0", "menor a 0" ó "igual a 0"
+    devuelve si el numero es "mayor a 0", "menor a 0" ó "igual a 0" realizando una comparacion con el 0
     '''
-    if(numero < 0):
-        return "numero negativo"
-    elif(numero > 0):
-        return "numero positivo"
+    return compara(numero,0)
+
+    
+def prueba():
+    numero = ingrese_numero()
+    signo_numero = signo(numero)
+    if(signo_numero < 0):
+        print("el numero es negativo (menor a 0)")
+    elif(signo_numero > 0):
+        print("el numero es positivo (mayor a 0)")
     else:
-        return "es el cero"
+        print("el numero es igual a 0")
+
+if __name__ == "__main__":
+    prueba()
