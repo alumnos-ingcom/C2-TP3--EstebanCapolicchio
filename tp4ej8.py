0446f56743a1b3de9cb39954f3e131503ec90324
EstebanCapolicchio <84167593+EstebanCapolicchio@users.noreply.github.com>
2021-06-01 22:28:13 -0300

    Finished ej8
    added testing function

--- tp4ej8.py
+++ tp4ej8.py
@@ -1,3 +1,6 @@
+from tp4ej1 import ingrese_numero
+
+#tp4ej8
 def ordenar_mayor_a_menor(uno, dos, tres):
     '''
     devuelve una lista con los tres elementos ordenada de mayor a menor
@@ -23,3 +26,21 @@ def ordenar_menor_a_mayor(uno, dos, tres):
             lista_aux[i+1] = lista_aux[i]
             lista_aux[i] = elem_aux
     return lista_aux 
+
+
+
+def prueba():
+    num1 = ingrese_numero("ingrese el primer numero")
+    num2 = ingrese_numero("ingrese el segundo numero")
+    num3 = ingrese_numero("ingrese el tercer numero")
+    
+    print("******* Ordenada de Menor a Mayor *******")
+    print(f"{ordenar_menor_a_mayor(num1,num2,num3)}")
+    print("*****************************************")
+    print("******* Ordenada de Mayor a Menor *******")
+    print(f"{ordenar_mayor_a_menor(num1,num2,num3)}")
+    print("*****************************************")
+    
+    
+if __name__ == "__main__":
+    prueba()
