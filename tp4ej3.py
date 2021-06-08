214f1bbd4d9e6cac366503faeb7571bf1045ebc2
EstebanCapolicchio <84167593+EstebanCapolicchio@users.noreply.github.com>
2021-06-08 18:10:45 -0300

    Finished ej3
    fixed:
    -"usa nombres descriptivos para las variables"

--- tp4ej3.py
+++ tp4ej3.py
@@ -13,12 +13,12 @@ def convertir_a_centigrados(fahrenheit):
     return (fahrenheit - 32) * 5/9
 
 def prueba():
-    FaC = ingrese_numero("ingrese numero para pasarlo de Farenheit a Celcius")
-    CaF = ingrese_numero("ingrese numero para pasarlo de Celcius a Farenheit")
-    convertido_FaC = convertir_a_centigrados(FaC)
-    convertido_CaF = convertir_a_fahrenheit(CaF)
-    print("|    Farenheit: "+str(FaC)" -->  Celcius: "+str(convertido_FaC)+"    |")
-    print("|    Celcius: "+str(CaF)" -->  Farenheit: "+str(convertido_CaF)+"    |")
+    valor_en_Farenheit = ingrese_numero("ingrese numero para pasarlo de Farenheit a Celcius")
+    valor_en_Celcius = ingrese_numero("ingrese numero para pasarlo de Celcius a Farenheit")
+    valor_en_Farenheit_convertido_en_Celcius = convertir_a_centigrados(valor_en_Farenheit)
+    valor_en_Celcius_convertido_en_Farenheit = convertir_a_fahrenheit(valor_en_Celcius)
+    print(f"|    Farenheit: {valor_en_Farenheit} -->  Celcius: {valor_en_Farenheit_convertido_en_Celcius}    |")
+    print(f"|    Celcius: {valor_en_Celcius} -->  Farenheit: {valor_en_Celcius_convertido_en_Farenheit}    |")
     
     
     
